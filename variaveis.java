5b595ca90b54c909250c1ca4a5c572bb9bf1f78a
Eduardo Santos <eduardosantospx@outlook.com>
2024-10-11 14:05:13 -0300

    tipos primitivos java

---
+++ variaveis.java
@@ -0,0 +1,19 @@
+public class variaveis{
+    public static void main(String[] args) {
+     //numeros inteiros
+     byte InteiroByte = 3 ;// 8 bites -128 a 127
+     short InteiroShort = 34; //16 bites -32.768 a 32.767
+     int Inteiro = 10000; //32 bites 
+     long InteiroLong= 10000000; // 64 bites -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807.
+
+     //numeros decimais
+     float peso = 5.8f; //32 bites precisão simples
+        double peso1 = 43.50; // 64.bites precisão dupla
+    
+    // strings representar palavras e cacacteres
+    String nome = "eduardo";
+    char sexo = 'm'; 
+        
+    }
+
+}
