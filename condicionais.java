2dba6d59de14e8e157e72bd93ee6a913736d11b8
Eduardo Santos <eduardosantospx@outlook.com>
2024-10-14 20:37:17 -0300

    condicionais java

---
+++ condicionais.java
@@ -0,0 +1,16 @@
+public class condicionais {
+    public static void main(String[] args) {
+    
+        boolean boo = true;
+        int num = 10;
+
+        if(boo){
+            System.out.println("verdade");
+        }if(num > 9 ){
+            System.out.println("maior que 9");
+        }else{
+            System.out.println("menor que 9");
+        }
+    }
+    
+}
