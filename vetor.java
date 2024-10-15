2e98a2c84c64b7bdab722d8ba647064a9d129078
Eduardo Santos <eduardosantospx@outlook.com>
2024-10-15 20:13:32 -0300

    vetor java

---
+++ vetor.java
@@ -0,0 +1,8 @@
+public class vetor {
+    public static void main(String[] args){
+        int[] colecasoInteiros = {1,2,3,4,5};// armazenando dessa forma ele se torna um array fixo ou seja não consegue mas acessar outros indices desse array
+        int[] meusNumeros = new int[90];// inicializa vazio com tamanho 90 0 á 89 não se pode inisializar ele sem tamanho
+        System.out.println(colecasoInteiros[0]);
+        System.out.println(meusNumeros.length);
+    }
+}
